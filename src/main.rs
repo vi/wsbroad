f4512e5bbf2c83052e47f23f4f862d1c6f53c295
Vitaly _Vi Shukela <vi0oss@gmail.com>
2017-09-18 03:07:19 +0300

    Multi-URL

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,13 @@ use std::rc::Rc;
 use std::cell::{RefCell};
 
 use compactmap::CompactMap;
+use std::collections::HashMap;
 
 type UsualClient = websocket::client::async::Framed<tokio_core::net::TcpStream, websocket::async::MessageCodec<websocket::OwnedMessage>>;
 
 type ClientSink = Rc<RefCell<futures::sync::mpsc::Sender<websocket::OwnedMessage>>>;
 type AllClients = Rc<RefCell<CompactMap<ClientSink>>>;
+type Url2Clientset = HashMap<String, AllClients>;
 
 #[async]
 fn serve_client(handle: Handle, client: UsualClient, all:AllClients) -> Result<(),()> {
@@ -56,8 +58,8 @@ fn serve_client(handle: Handle, client: UsualClient, all:AllClients) -> Result<(
             OwnedMessage::Pong(_) => continue,
             _ => m,
         };
-        use std::ops::Deref;
-        for (id, i) in all.borrow().deref().into_iter() {
+        
+        for (id, i) in all.borrow().iter() {
             if id == my_id { continue; }
             
             // Send if possible, throw away message if not ready
@@ -83,7 +85,7 @@ fn main() {
 
     let str = server.incoming().map_err(|_|());
     
-    let all_clients : AllClients = Rc::new(RefCell::new(CompactMap::new()));
+    let mapping : Rc<RefCell<Url2Clientset>> = Rc::new(RefCell::new(HashMap::new()));
     
     let f = async_block! {
         #[async]
@@ -91,10 +93,37 @@ fn main() {
             let url = format!("{}", upgrade.request.subject.1);
             println!("+ {} -> {}", addr, url);
             let (addr2, url2) = (addr.clone(), url.clone());
+            
             let (client, _headers) = await!(upgrade.accept().map_err(|_|()))?;
-            let f = serve_client(handle.clone(), client, all_clients.clone());
+            
+            let map_ = mapping.clone();
+            let map2_ = mapping.clone();
+            let mut map = map_.borrow_mut();
+            
+            if !map.contains_key(&*url) {
+                let new_clientset : AllClients = Rc::new(RefCell::new(CompactMap::new()));
+                map.insert(url.clone(), new_clientset);
+                println!("New URL: {}", url);
+            }
+            
+            let clientset : AllClients = map.get(&*url).unwrap().clone();
+            
+            let f = serve_client(handle.clone(), client, clientset);
+            
             handle.spawn(f.map_err(|_|()).map(move |_|{
                 println!("- {} -> {}", addr2, url2);
+                
+                let mut map = map2_.borrow_mut();
+                let mut do_remove = false;
+                if let Some(x) = map.get(&*url2) {
+                    if (*x).borrow().len_slow() == 0 {
+                        println!("Expiring URL: {}", url2);
+                        do_remove = true;
+                    }
+                }
+                if do_remove {
+                    map.remove(&*url2);
+                }
             }));
         }
         Ok::<(),()>(())
